2acf385b8f4f9e071775825ea8bf85de0419a254
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-17 20:33:21 +0300

    GETIR APP

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
           'mobile-app': 'url(https://getir.com/_next/static/images/doodle-d659f9f1fd505c811c2331fe3ffddd5f.png)'
         }),
         colors: {
-          'brand-color': '#4c3398',
+          'brand-color': '#5d3ebc',
           'primary-brand-color': '#5d3ebc',
           'secondary-brand-color': '#7849f7',
           'brand-yellow': '#ffd300'
