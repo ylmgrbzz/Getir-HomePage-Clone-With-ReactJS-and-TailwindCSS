319d59bca5237d95fdc09be9f74555366b099c0c
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-17 21:35:10 +0300

    GETIR APP

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   return (
     <>
       <Header />
-      <Campaigns />
       <HeroSection />
       <Categories />
       <Campaigns />
--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import categoryData from '../api/categories.json'
+import Category from '../components/ui/Category'
+import Title from '../components/ui/Title'
 
-const Categories = () => {
+export default function Categories() {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        // istek
+        // setTimeout(() => setCategories(categoryData), 1000)
+        setCategories(categoryData)
+    }, [])
+
     return (
-        <div>Categories</div>
+        <div className="bg-white py-4">
+            <div className="container mx-auto">
+                <Title>Kategoriler</Title>
+                <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 2xl:grid-cols-12">
+                    {/* {!categories.length && 'Yükleniyor..'} */}
+                    {categories && categories.map((category, index) => <Category key={index} category={category} />)}
+                </div>
+            </div>
+        </div>
     )
 }
-
-export default Categories
