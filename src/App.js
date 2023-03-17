69e8631ba3c9434b0d683a6f045f23e26b6e8563
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-17 20:08:21 +0300

    GETIR APP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import Header from './components/Header'
+import HeroSection from './components/HeroSection'
+import Categories from './components/Categories'
+import Campaigns from './components/Campaigns'
+import Favorites from './components/Favorites'
+import MobileApp from './components/MobileApp'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      ylm
-    </div>
+    <>
+      <Header />
+      <Campaigns />
+      <HeroSection />
+      <Categories />
+      <Campaigns />
+      <div className="container mx-auto grid gap-y-6 pt-8">
+        <Favorites />
+        <MobileApp />
+        <Cards />
+      </div>
+      <Footer />
+    </>
   );
 }
 
