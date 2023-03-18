8fef6794f4f2874d524bce0ec11f8b1cc61d0769
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-18 17:54:27 +0300

    GETIR APP

--- src/components/MobileApp.js
+++ src/components/MobileApp.js
@@ -1,9 +1,35 @@
-import React from 'react'
-
-const MobileApp = () => {
+export default function MobileApp() {
     return (
-        <div>MobileApp</div>
-    )
+        <div className="bg-primary-brand-color flex flex-col md:flex-row justify-between items-center bg-mobile-app md:rounded-lg text-white">
+            <div className="flex flex-col gap-y-3 p-10 text-center md:text-left">
+                <h3 className="text-2xl font-bold tracking-tight">Getir'i indirin!</h3>
+                <p className="font-semibold">
+                    İstediğiniz ürünleri dakikalar içinde kapınıza <br /> getirelim.
+                </p>
+                <nav className="mt-5 flex flex-wrap lg:flex-nowrap justify-center gap-2">
+                    <a href="#" className="transition-all transform hover:scale-105">
+                        <img
+                            src="https://cdn.getir.com/getirweb-images/common/landing/phoneLanding.png"
+                            className="md:h-8 lg:h-auto"
+                        />
+                    </a>
+                    <a href="#" className="transition-all transform hover:scale-105">
+                        <img
+                            src="https://cdn.getir.com/getirweb-images/common/landing/phoneLanding.png"
+                            className="md:h-8 lg:h-auto"
+                        />
+                    </a>
+                    <a href="#" className="transition-all transform hover:scale-105">
+                        <img
+                            src="https://cdn.getir.com/getirweb-images/common/landing/phoneLanding.png"
+                            className="md:h-8 lg:h-auto"
+                        />
+                    </a>
+                </nav>
+            </div>
+            <picture className="pt-6 hidden md:block md:self-end">
+                <img src="https://getir.com/_next/static/images/phoneLanding-88c033545710c4808054072689e187d7.png" />
+            </picture>
+        </div>
+    );
 }
-
-export default MobileApp
