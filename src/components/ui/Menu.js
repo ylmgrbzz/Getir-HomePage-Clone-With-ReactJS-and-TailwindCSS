efa4cd54491fe57c70454ec94e56d97e0d277f84
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-18 18:17:07 +0300

    GETIR APP

--- src/components/ui/Menu.js
+++ src/components/ui/Menu.js
@@ -1,35 +1,40 @@
-import { useState, useEffect } from 'react'
-import { Collapse } from 'react-collapse';
-import { useWindowWidth } from '@react-hook/window-size'
-import { IoIosArrowDown } from 'react-icons/io';
+import { useState, useEffect } from "react";
+import { Collapse } from "react-collapse";
+import { useWindowWidth } from "@react-hook/window-size";
+import { IoIosArrowDown } from "react-icons/io";
 
 export default function Menu({ title, items }) {
-
-    const windowWidth = useWindowWidth()
+    const windowWidth = useWindowWidth();
     const [isOpen, setIsOpen] = useState(true);
 
     const toggleCollapse = () => {
         if (windowWidth <= 768) {
             setIsOpen(!isOpen);
         }
-    }
+    };
 
     useEffect(() => {
         if (isOpen && windowWidth <= 768) {
-            setIsOpen(false)
+            setIsOpen(false);
         }
         if (!isOpen && windowWidth > 768) {
-            setIsOpen(true)
+            setIsOpen(true);
         }
-    }, [windowWidth])
+    }, [windowWidth]);
 
     return (
         <section>
             <nav className="grid gap-y-2 md:gap-y-4">
-                <h6 onClick={toggleCollapse} className="text-lg text-primary-brand-color flex items-center justify-between">
+                <h6
+                    onClick={toggleCollapse}
+                    className="text-lg text-primary-brand-color flex items-center justify-between"
+                >
                     {title}
                     <button className="grid md:hidden w-6 h-6 place-items-center rounded-lg bg-primary-brand-color bg-opacity-10 text-primary-brand-color">
-                        <span className={`transition-all transform ${isOpen ? '-rotate-180' : ''}`}>
+                        <span
+                            className={`transition-all transform ${isOpen ? "-rotate-180" : ""
+                                }`}
+                        >
                             <IoIosArrowDown size={14} />
                         </span>
                     </button>
@@ -49,5 +54,5 @@ export default function Menu({ title, items }) {
                 </Collapse>
             </nav>
         </section>
-    )
+    );
 }
